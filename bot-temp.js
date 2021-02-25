ab3fe21ff33f4fa429c4d8ccc77bd53c36db7b83
Stecca <a.stecca@protonmail.com>
2021-02-26 00:06:58 +0100

    Reworked functions

--- bot-temp.js
+++ bot-temp.js
@@ -4,7 +4,8 @@ const puppeteer = require('puppeteer');//headless browser emulator
 const https = require('https');
 const request = require('request');
 
-const frequency = 30000;
+//TODO: mettere i campi su un config.json
+const frequency = 10000;
 const inputWaiting = 4000;
 const emailInputWait = 200;
 const emailSendCode = 15000;
@@ -18,7 +19,22 @@ if (process.argv.length >= 3){
   link = myArgs[0];
 }
 
-function makeid(length) {//generates a casual id
+async function getProxy() {
+  request("https://raw.githubusercontent.com/scidam/proxy-list/master/proxy.json", {json: true}, (error, res, body) => {
+    if (error) {
+        return console.log(error);
+    };
+    if (!error && res.statusCode == 200) {
+        rand = Math.floor(Math.random() * 1000);
+        const proxy = body.proxies[rand].ip + ':' + body.proxies[rand].port;
+        console.log(proxy);
+        return proxy;
+    };
+  });
+}
+
+//generates a casual id
+function makeid(length) {
   let result           = "";
   const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   const charactersLength = characters.length;
@@ -28,8 +44,9 @@ function makeid(length) {//generates a casual id
   return result;
 }
 
+//blocks images ,fonts and css on the page
 async function speedup(page){
-  await page.setRequestInterception(true);//blocks images ,fonts and css
+  await page.setRequestInterception(true);
   page.on('request', (request) => {
     if (request.resourceType() === 'image') request.abort();
     else if (request.resourceType() === 'media') request.abort();
@@ -39,38 +56,19 @@ async function speedup(page){
   });
 }
 
+//gets a random email
 async function getMail(tempmail){
   await tempmail.goto("https://temp-mail.org/", {
       waitUntil: 'networkidle2',
-    });
+    })
 
   await new Promise(r => setTimeout(() => r(), 8000));
   const mail = await tempmail.$eval('input#mail.emailbox-input', el => el.value); //gets mail
   return mail;
 }
 
-async function main(browser){
-  const pages = await browser.pages();
-  const sugargoo = pages[0];
-  const tempmail = await browser.browserContexts()[0].newPage();
-
-  await speedup(sugargoo);
-  await speedup(tempmail);
-
-  sugargoo.setViewport({ width: 900, height: 900 });
-  tempmail.setViewport({ width: 900, height: 900 });
-
-  //opens the pages asyncronously
-  promise1 = sugargoo.goto(link, {
-    waitUntil: 'networkidle2',
-  });
-  const email = await getMail(tempmail);
-    
-  //generates a fake id
-  const user = makeid(5);
-  await new Promise(r => setTimeout(() => r(), inputWaiting));
-
-  console.log(user)
+//fills sugargoo page
+async function sugarFill(sugargoo, user, email) {
   await sugargoo.waitForSelector('input[name=username]');
   await sugargoo.$eval('input[name=username]', (el, value) => el.value = value, user);
   await sugargoo.waitForSelector('input[name=email]');
@@ -85,38 +83,78 @@ async function main(browser){
   await sugargoo.click('a[data-type=email]');
 
   await new Promise(r => setTimeout(() => r(), waitForCode));
+  return;
+}
 
-  //---------------------------------------------------------------------------------------------------------------------bestemmie
-  console.log(`opening ${email} inbox`);
+//gets code
+async function getCode(tempmail, browser){
+    //moves the mouse randomly to trigger MouseEvent
+    let intervalId = setInterval( () => {
+      tempmail.mouse.move(Math.floor(Math.random() * 100) , Math.floor(Math.random() * 100));
+    },1000);
   
-  //moves the mouse randomly to trigger MouseEvent
-  let intervalId = setInterval( () => {
-    tempmail.mouse.move(Math.floor(Math.random() * 100) , Math.floor(Math.random() * 100));
-  },1000);
+    await tempmail.waitForSelector('span[title="noreply@sugargoo.com"]', {timeout: 0});
+    clearInterval(intervalId);
+    //console.log("mail caricata");
+    await tempmail.click('span[title="noreply@sugargoo.com"]');
+    await tempmail.waitForSelector('.inbox-data-content-intro');
+    await new Promise(r => setTimeout(() => r(), 6000));
+    const value = await tempmail.$eval('.inbox-data-content-intro', el => el.innerText);
+    let code = 0;
+    try{
+      code = await value.split(":")[2].slice(0, 4)
+      await tempmail.close();
+    }catch{
+      console.log("Ci vuole più tempo per caricare la mail");
+      browser.close();
+      return;
+    };
+    console.log(code);
+    return code;
+}
 
-  await tempmail.waitForSelector('span[title="noreply@sugargoo.com"]', {timeout: 0});
-  clearInterval(intervalId);
-  //console.log("mail caricata");
-  await tempmail.click('span[title="noreply@sugargoo.com"]');
-  await tempmail.waitForSelector('.inbox-data-content-intro');
-  await new Promise(r => setTimeout(() => r(), 6000));
-  const value = await tempmail.$eval('.inbox-data-content-intro', el => el.innerText);
-  let code = "";
-  try{
-    code = await value.split(":")[2].slice(0, 4)}
-  catch{
-    console.log("Ci vuole più tempo per caricare la mail");
-    browser.close();
-    return;
-  };
-  console.log(code);
-
-  await tempmail.close();
+//fills the code
+async function confirmCode(sugargoo, code) {
   await sugargoo.waitForSelector('input[name=captcha]');
   await new Promise(r => setTimeout(() => r(), codeFillTime));
   await sugargoo.$eval('input[name=captcha]', (el, value) => el.value = value, code);
   await (await sugargoo.waitForSelector('input[name=checkbox]')).click();
   await (await sugargoo.waitForSelector('input[type=submit]')).click();
+}
+
+async function main(){
+  //lancia puppeteer senza "grafica" e in incognito
+  const browser = await puppeteer.launch({headless: false, args: [`--incognito`]});
+  const pages = await browser.pages();
+  const sugargoo = pages[0];
+  const tempmail = await browser.newPage();
+
+  await speedup(sugargoo);
+  await speedup(tempmail);
+
+  sugargoo.setViewport({ width: 900, height: 900 });
+  tempmail.setViewport({ width: 900, height: 900 });
+
+  //opens the pages asyncronously
+ sugargoo.goto(link, {
+    waitUntil: 'networkidle2',
+  });
+  const email = await getMail(tempmail);
+    
+  //generates a fake id
+  const user = makeid(5);
+  await new Promise(r => setTimeout(() => r(), inputWaiting));
+
+  console.log(user);
+  //fills sugargoo page
+  await sugarFill(sugargoo, user, email);
+
+  console.log(`opening ${email} inbox`);
+  //gets the code from tempmail
+  const code = await getCode(tempmail, browser);
+
+  //fills the code
+  await confirmCode(sugargoo, code);
 
   await new Promise(r => setTimeout(() => r(), waitToQuit))
   browser.close();
@@ -125,6 +163,5 @@ async function main(browser){
 }
 
 setInterval(async function () {
-  const browser = await puppeteer.launch({headless: true, args: ['--incognito']});//lancia puppeteer senza "grafica" e in incognito
-  main(browser);
+    main();
 }, frequency);
