32aeea8a6baba7f46ba2657ad912516c14b04b0e
McStecca <andreastecca2001@gmail.com>
2021-03-16 13:23:00 +0100

    Added config file

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {};
+config.frequency = 300000;
+config.frequencyRandomness = 30000
+config.inputWaiting = 4000;
+config.emailInputWait = 200;
+config.emailSendCode = 15000;
+config.waitForCode = 5000;
+config.codeFillTime = 500;
+config.waitToQuit = 2000;
+
+module.exports = config;
